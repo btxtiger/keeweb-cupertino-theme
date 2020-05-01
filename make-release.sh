b6a6ab0b2fcdc5dd4ce3b21f7553c429555cb5fd
btxtiger <btxtiger@outlook.com>
2020-05-01 09:13:15 +0200

    Refactor theme names

--- make-release.sh
+++ make-release.sh
@@ -3,8 +3,8 @@
 npm run format-prettier
 npm run scss:compile
 
-cat dist/theme-auto.css > release/keeweb-cupertino-theme-auto/plugin.css
-cat dist/theme-dark.css > release/keeweb-cupertino-theme-dark/plugin.css
-cat dist/theme-light.css > release/keeweb-cupertino-theme-light/plugin.css
+cat dist/theme-auto.css > release/theme-cupertino-auto/plugin.css
+cat dist/theme-dark.css > release/theme-cupertino-dark/plugin.css
+cat dist/theme-light.css > release/theme-cupertino-light/plugin.css
 
 exit 0
